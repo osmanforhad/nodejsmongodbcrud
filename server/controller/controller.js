b98d7e6b046009a14a3d9c115919ecc6312f3130
Osman Forhad <osmanforhad93@gmail.com>
2021-04-11 12:14:31 +0600

    setup CRUD API for the app

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+//create and save new user
+exports.create = (req, res) => {
+
+};
+
+//retribe and return all users / retribe and return a single user
+exports.find() = (req, res) => {
+
+};
+
+//update a new identified user by user id
+exports.update() = (req, res) => {
+
+};
+
+//Delete a user with specified user id in the request
+exports.delete() = (req, res) => {
+
+};
