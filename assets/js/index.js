24f11d529763396ed78533107679f819aff8bc74
Osman Forhad <osmanforhad93@gmail.com>
2021-04-12 13:05:24 +0600

    working delete operation and finished the node js mongo db CRUD API application

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ $("#update_user").submit(function(event) {
         data[n['name']] = n['value']
     })
 
-    console.log(data);
+    //console.log(data);
 
     var request = {
         "url": `http://localhost:3000/api/users/${data.id}`,
@@ -24,3 +24,22 @@ $("#update_user").submit(function(event) {
         alert("Data Updated Successfully!");
     })
 })
+
+if (window.location.pathname == "/") {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function() {
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method": "DELETE"
+        }
+
+        if (confirm("Do you really want to delete this record?")) {
+            $.ajax(request).done(function(response) {
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+    })
+}
